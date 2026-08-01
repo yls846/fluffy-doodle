5ab6ada77123510f9aaf03d1b404c2b14eb521af
yls846 <213882192@qq.com>
2026-08-01 15:00:22 +0800

    更新 ControlsOptionsScreenMixin.java

--- src/main/java/dev/xef2/visualkeymap/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/dev/xef2/visualkeymap/mixin/ControlsOptionsScreenMixin.java
@@ -10,27 +10,25 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Screen.class)
+@Mixin(ControlsOptionsScreen.class)
 public abstract class ControlsOptionsScreenMixin extends Screen {
 
     protected ControlsOptionsScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
+    // 硬编码方法描述符，不依赖 refmap
+    @Inject(method = "init()V", at = @At("TAIL"))
     private void addVisualKeymapButton(CallbackInfo ci) {
-        // 仅在 ControlsOptionsScreen 里添加按钮
-        if ((Object) this instanceof ControlsOptionsScreen) {
-            ButtonWidget button = ButtonWidget.builder(
-                    Text.literal("Visual Keymap"),
-                    btn -> {
-                        if (this.client != null) {
-                            this.client.setScreen(new VisualKeymapScreen(this));
-                        }
+        ButtonWidget button = ButtonWidget.builder(
+                Text.literal("Visual Keymap"),
+                btn -> {
+                    if (this.client != null) {
+                        this.client.setScreen(new VisualKeymapScreen(this));
                     }
-            ).dimensions(this.width / 2 - 100, this.height - 28, 200, 20).build();
+                }
+        ).dimensions(this.width / 2 - 100, this.height - 28, 200, 20).build();
 
-            this.addDrawableChild(button);
-        }
+        this.addDrawableChild(button);
     }
 }
