e53eaa33cb55000685e7940e5ef782fd9de09a82
yls846 <213882192@qq.com>
2026-08-01 14:28:19 +0800

    更新 ControlsOptionsScreen.java

--- src/main/java/dev/xef2/visualkeymap/gui/screen/ControlsOptionsScreen.java
+++ src/main/java/dev/xef2/visualkeymap/gui/screen/ControlsOptionsScreen.java
@@ -22,12 +22,16 @@ public class ControlsOptionsScreen extends Screen {
         gridWidget.getMainPositioner().marginX(5).marginBottom(4).alignHorizontalCenter();
         GridWidget.Adder adder = gridWidget.createAdder(2);
 
+        // 注意：此处 SharedData 和 Consumer 传 null / 空实现，因为这个界面只展示不交互
         KeybindsListWidget keybindsList = new KeybindsListWidget(
-            this.client, this.width, this.height - 64, 32, this.height - 32, 20
+            this.client, this.width, this.height - 64, 32, 20,
+            null,
+            kb -> {}
         );
         this.addSelectableChild(keybindsList);
 
-        adder.add(ButtonWidget.builder(ScreenTexts.DONE, button -> this.close()).width(200).build(), 2, adder.copyPositioner().marginTop(6));
+        adder.add(ButtonWidget.builder(ScreenTexts.DONE, button -> this.close()).width(200).build(),
+                2, adder.copyPositioner().marginTop(6));
 
         gridWidget.refreshPositions();
         SimplePositioningWidget.setPos(gridWidget, 0, this.height - 48, this.width, 64);
