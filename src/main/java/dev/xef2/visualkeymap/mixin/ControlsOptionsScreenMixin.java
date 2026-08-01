dcfa73fe89e2412c85d59e00056eba19d8958efd
yls846 <213882192@qq.com>
2026-08-01 14:24:40 +0800

    更新 ControlsOptionsScreenMixin.java

--- src/main/java/dev/xef2/visualkeymap/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/dev/xef2/visualkeymap/mixin/ControlsOptionsScreenMixin.java
@@ -1,7 +1,8 @@
 package dev.xef2.visualkeymap.mixin;
 
-import dev.xef2.visualkeymap.gui.screen.ControlsOptionsScreen;
+import dev.xef2.visualkeymap.gui.screen.VisualKeymapScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(net.minecraft.client.gui.screen.option.ControlsOptionsScreen.class)
-public class ControlsOptionsScreenMixin extends Screen {
+@Mixin(ControlsOptionsScreen.class)
+public abstract class ControlsOptionsScreenMixin extends Screen {
 
     protected ControlsOptionsScreenMixin(Text title) {
         super(title);
@@ -19,12 +20,12 @@ public class ControlsOptionsScreenMixin extends Screen {
     @Inject(method = "init", at = @At("TAIL"))
     private void addVisualKeymapButton(CallbackInfo ci) {
         ButtonWidget button = ButtonWidget.builder(
-            Text.literal("Visual Keymap"),
-            btn -> {
-                if (this.client != null) {
-                    this.client.setScreen(new ControlsOptionsScreen(this));
+                Text.literal("Visual Keymap"),
+                btn -> {
+                    if (this.client != null) {
+                        this.client.setScreen(new VisualKeymapScreen(this));
+                    }
                 }
-            }
         ).dimensions(this.width / 2 - 100, this.height - 28, 200, 20).build();
 
         this.addDrawableChild(button);
