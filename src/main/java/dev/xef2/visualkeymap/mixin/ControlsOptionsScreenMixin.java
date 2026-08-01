9469033aabd5aa1bc523aa9a60127c6ce38d498a
yls846 <213882192@qq.com>
2026-08-01 14:11:38 +0800

    更新 ControlsOptionsScreenMixin.java

--- src/main/java/dev/xef2/visualkeymap/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/dev/xef2/visualkeymap/mixin/ControlsOptionsScreenMixin.java
@@ -1,11 +1,7 @@
 package dev.xef2.visualkeymap.mixin;
 
-import dev.xef2.visualkeymap.VisualKeymap;
-import dev.xef2.visualkeymap.gui.screen.VisualKeymapScreen;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
+import dev.xef2.visualkeymap.gui.screen.ControlsOptionsScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,24 +9,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Environment(EnvType.CLIENT)
-@Mixin(ControlsOptionsScreen.class)
-public class ControlsOptionsScreenMixin {
+@Mixin(net.minecraft.client.gui.screen.option.ControlsOptionsScreen.class)
+public class ControlsOptionsScreenMixin extends Screen {
+
+    protected ControlsOptionsScreenMixin(Text title) {
+        super(title);
+    }
 
     @Inject(method = "init", at = @At("TAIL"))
     private void addVisualKeymapButton(CallbackInfo ci) {
-        ControlsOptionsScreen self = (ControlsOptionsScreen) (Object) this;
-        MinecraftClient client = MinecraftClient.getInstance();
-
         ButtonWidget button = ButtonWidget.builder(
-                Text.translatable(VisualKeymap.getTranslationKey("gui.open_keymap")),
-                (btn) -> client.setScreen(new VisualKeymapScreen(
-                        self,
-                        client.options
-                ))
-        ).dimensions(self.width / 2 - 100, self.height / 6 + 144 - 26, 200, 20).build();
+            Text.literal("Visual Keymap"),
+            btn -> {
+                if (this.client != null) {
+                    this.client.setScreen(new ControlsOptionsScreen(this));
+                }
+            }
+        ).dimensions(this.width / 2 - 100, this.height - 28, 200, 20).build();
 
-        // Use addDrawableChild to add the button; available via the Screen base class
-        ((net.minecraft.client.gui.screen.Screen) (Object) this).addDrawableChild(button);
+        this.addDrawableChild(button);
     }
 }
